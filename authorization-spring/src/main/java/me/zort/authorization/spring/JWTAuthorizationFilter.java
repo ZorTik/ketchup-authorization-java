d42656151dc58a2adf5ae75a405c3e00b96123cd
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-08-12 01:33:27 +0200

    Add permission mapping to JWTAuthorizationFilter

--- authorization-spring/src/main/java/me/zort/authorization/spring/JWTAuthorizationFilter.java
+++ authorization-spring/src/main/java/me/zort/authorization/spring/JWTAuthorizationFilter.java
@@ -3,7 +3,10 @@ package me.zort.authorization.spring;
 import lombok.RequiredArgsConstructor;
 import me.zort.authorization.lib.AuthorizationClient;
 import me.zort.authorization.lib.model.UserDetails;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,11 +14,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@RequiredArgsConstructor
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     private final AuthorizationClient client;
+    private final Map<AntPathRequestMatcher, String> permissionMapping = new ConcurrentHashMap<>();
+
+    public JWTAuthorizationFilter(AuthorizationClient client) {
+        this(client, null);
+    }
+
+    public JWTAuthorizationFilter(AuthorizationClient client, @Nullable PermissionMapping mapping) {
+        this.client = client;
+        if (mapping != null) {
+            this.permissionMapping.putAll(mapping.getMapping());
+        }
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -26,8 +43,10 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             if (session.authorized()) {
                 try {
                     UserDetails userDetails = session.fetchUserDetails();
-                    KetchupAuthenticationToken authenticationToken = new KetchupAuthenticationToken(session, userDetails);
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    if (permissionNodeCheck(request, session)) {
+                        KetchupAuthenticationToken authenticationToken = new KetchupAuthenticationToken(session, userDetails);
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
                 } catch (AuthorizationClient.UnauthorizedException ignored) {
                 }
             }
@@ -36,5 +55,32 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         // Combine with spring security's WebSecurityConfigurerAdapter to handle
         // requests where SecurityContextHolder.getContext().getAuthentication() is not present.
         filterChain.doFilter(request, response);
+    }
+
+    private boolean permissionNodeCheck(HttpServletRequest request, AuthorizationClient.Session session) {
+        for (AntPathRequestMatcher matcher : permissionMapping.keySet()) {
+            try {
+                if (matcher.matches(request) && !session.fetchNodeState(permissionMapping.get(matcher))) {
+                    return false;
+                }
+            } catch (AuthorizationClient.UnauthorizedException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static class PermissionMapping {
+        private final Map<AntPathRequestMatcher, String> mapping = new HashMap<>();
+
+        public @NotNull PermissionMapping path(String ant, String node) {
+            mapping.put(new AntPathRequestMatcher(ant), node);
+            return this;
+        }
+
+        public @NotNull Map<AntPathRequestMatcher, String> getMapping() {
+            return new HashMap<>(mapping);
+        }
     }
 }
